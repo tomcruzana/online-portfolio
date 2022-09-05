f082a7029abde48f7dcf69aa012c87d473807e98
tomcruzana <tomcruzana@ymail.com>
2022-09-05 03:58:05 -0700

    added work experience instance

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -10,26 +10,5 @@ export class ExperienceComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  IMG_PATH: String = '../../../assets/';
-
-  educations: any[] = [
-    {
-      thumb: this.IMG_PATH + 'apus.png',
-      degree: 'MS Information Technology, Software Development',
-      school: 'american public university',
-      year: '2020',
-    },
-    {
-      thumb: this.IMG_PATH + 'cap.png',
-      degree: 'BS Information Technology',
-      school: 'CAP college',
-      year: '2017',
-    },
-    {
-      thumb: this.IMG_PATH + 'sti.png',
-      degree: 'Diploma in Computer and Electronics Technology',
-      school: 'system technology institute college',
-      year: '2011',
-    },
-  ];
+  @Input() experiences: any[] = [];
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,8 +11,11 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {}
   name: string = 'thomas cruzana';
   titles: string[] = ['software developer, web developer'];
+  IMG_PATH: String = '../../../assets/';
+
   summary: string =
     'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.';
+
   contactInfo: any = {
     github: 'https://github.com/tomcruzana',
     linkedin: 'linkedin.com/tomcruzana',
@@ -20,4 +23,52 @@ export class HeaderComponent implements OnInit {
     email: 'thomascruzana@gmail.com',
     phone: '3609322458',
   };
+
+  educationExperiences: any[] = [
+    {
+      thumb: this.IMG_PATH + 'apus.png',
+      degree: 'MS Information Technology, Software Development',
+      school: 'american public university',
+      year: '2020',
+    },
+    {
+      thumb: this.IMG_PATH + 'cap.png',
+      degree: 'BS Information Technology',
+      school: 'CAP college',
+      year: '2017',
+    },
+    {
+      thumb: this.IMG_PATH + 'sti.png',
+      degree: 'Diploma in Computer and Electronics Technology',
+      school: 'system technology institute college',
+      year: '2011',
+    },
+  ];
+
+  workExperiences: any[] = [
+    {
+      thumb: this.IMG_PATH + 'infosys.png',
+      degree: 'Associate Software Developer',
+      school: 'Infosys Ltd',
+      year: '2021-2022',
+    },
+    {
+      thumb: this.IMG_PATH + 'custom.png',
+      degree: 'Configuration Leader',
+      school: 'Custom America Inc',
+      year: '2017 - 2020',
+    },
+    {
+      thumb: this.IMG_PATH + 'sutherland.png',
+      degree: 'Consultant',
+      school: 'Sutherland Global Services',
+      year: '2013 - 2014',
+    },
+    {
+      thumb: this.IMG_PATH + 'stream.png',
+      degree: 'Technical Support Professional',
+      school: 'Stream Global Services',
+      year: '2011 - 2012',
+    },
+  ];
 }
